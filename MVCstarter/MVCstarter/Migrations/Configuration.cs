42deade0a483a5b0ec49db767acbd268e3cd45be
Ben-Donnellan35 <Ben.Donnellan@outlook.com>
2018-04-09 16:47:23 +0100

    adding a new field

--- MVCstarter/MVCstarter/Migrations/Configuration.cs
+++ MVCstarter/MVCstarter/Migrations/Configuration.cs
@@ -21,6 +21,7 @@ namespace MVCstarter.Migrations
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-1-11"),
                     Genre = "Romantic Comedy",
+                    Rating = "PG",
                     Price = 7.99M
                 },
 
